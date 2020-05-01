f7c836dab6f1bea6a751688ea18f4ec6bce4a045
tushryadav <tusharyadav511@gmail.com>
2020-05-01 19:11:08 +0530

    My First Commit
    My firest Commit

---
+++ app/src/main/java/com/switchonkannada/switchon/logInOption.kt
@@ -0,0 +1,12 @@
+package com.switchonkannada.switchon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class logInOption : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in_option)
+    }
+}
